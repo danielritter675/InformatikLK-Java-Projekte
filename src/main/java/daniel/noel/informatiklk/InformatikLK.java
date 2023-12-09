f06c30f0f03e58d8a529d567507b2127a67a1667
Daniel Ritter <87754989+DetDanny@users.noreply.github.com>
2023-12-10 00:47:54 +0100

    Programmstart geändert

--- src/main/java/daniel/noel/informatiklk/InformatikLK.java
+++ src/main/java/daniel/noel/informatiklk/InformatikLK.java
@@ -12,12 +12,25 @@ import javax.swing.JFrame;
 public class InformatikLK {
 
     public static void main(String[] args) {
-        AB00_HelloWorld helloWorldGUI = new AB00_HelloWorld();
-        AB01_TempRechner tempRechnerGUI = new AB01_TempRechner();
-        AB02_Zahlenraten zahlenratenGUI = new AB02_Zahlenraten();
-        AB03_Potenzrechner potenzrechnerGUI = new AB03_Potenzrechner();
-        AB04_TicTacToe ticTacToeGUI = new AB04_TicTacToe();
-        AB06_SechsAusNeunundvierzig lottogeneratorGUI = new AB06_SechsAusNeunundvierzig();
-        lottogeneratorGUI.setVisible(true);
+        //AB00_HelloWorld helloWorldGUI = new AB00_HelloWorld();
+        //helloWorldGUI.setVisible(true);
+        
+        //AB01_TempRechner tempRechnerGUI = new AB01_TempRechner();
+        //tempRechnerGUI..setVisible(true);
+        
+        //AB02_Zahlenraten zahlenratenGUI = new AB02_Zahlenraten();
+        //zahlenratenGUI.setVisible(true);
+        
+        //AB03_Potenzrechner potenzrechnerGUI = new AB03_Potenzrechner();
+        //potenzrechnerGUI.setVisible(true);
+        
+        //AB04_TicTacToe ticTacToeGUI = new AB04_TicTacToe();
+        //ticTacToeGUI.setVisible(true);
+        
+        //AB06_SechsAusNeunundvierzig lottogeneratorGUI = new AB06_SechsAusNeunundvierzig();
+        //lottogeneratorGUI.setVisible(true);
+        
+        AB09_Fibonacci fibonacciGUI = new AB09_Fibonacci();
+        fibonacciGUI.setVisible(true);
     }
 }
